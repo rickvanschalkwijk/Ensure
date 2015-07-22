cf1cd58a71990b84dedf051828f33a677e940645
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2015-07-22 17:36:49 +0200

    init bitbucket commit

--- Source/Ensure.UnitTests/Class1.cs
+++ Source/Ensure.UnitTests/Class1.cs
@@ -1,7 +1,18 @@
-﻿namespace Ensure.UnitTests
+﻿using Ensure.Extensions;
+
+using NUnit.Framework;
+
+namespace Ensure.UnitTests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+       public void MethodName_ShouldDo_ReturnValue()
+        {
+            object value = null;
 
+            Ensure.That(value, "object").IsNotNull();   
+       }
     }
 }
---
+++ Source/Ensure/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ensure.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void IsNotNull(this Param<object> param)
+        {
+            if (param.Value == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
